9bf59d030ff1b1394f8ebd9733ce799892e1eb02
Vickoman <vicmandlagasca@gmail.com>
2021-02-23 13:26:34 -0500

    singleton test

---
+++ src/design-patterns/creationals/singleton/singleton.spec.ts
@@ -0,0 +1,21 @@
+import MyConfiguration from "./index"
+import { expect } from "chai";
+import "mocha";
+
+let config = MyConfiguration.getInstance();
+
+describe("My Configuration class", ()=> {
+
+    it("Objects should not be null", () => {
+        expect( config !== null).to.equal(true);
+    });
+
+    it("Objects should be equals", () => {
+        let config2 = MyConfiguration.getInstance();
+        expect(config === config2).to.equal(true);
+    });
+
+    it("environment property should be development", () => {
+        expect(config.getEnvironment === "development").to.equal(true);
+    })
+});
